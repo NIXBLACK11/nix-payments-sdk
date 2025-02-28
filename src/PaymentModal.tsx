99e60ed3277064ff8dd466851fabf459104eadee
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-02-28 14:17:17 +0530

    update unecessary

--- src/PaymentModal.tsx
+++ src/PaymentModal.tsx
@@ -1,6 +1,5 @@
 import { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
